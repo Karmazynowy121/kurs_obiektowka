3d892377688ac84c64e2a0628a9d08ec0b8892cf
Karmazynowy121 <104931543+Karmazynowy121@users.noreply.github.com>
2022-09-26 19:53:45 +0200

    Add files via upload
    eee

---
+++ Bankpustegonasienia/Bankpustegonasienia/Logowanie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankpustegonasienia
+{
+    public class Logowanie
+    {
+        public string Name;
+        public string Password;
+
+        public bool RememberMe;
+        public int AccNumber;
+        public Logowanie(string name, string password, bool rememberMe, int accNumber)
+        {
+            Name = name;
+            Password = password;
+            RememberMe = rememberMe;
+            AccNumber = accNumber;
+
+        }
+
+    }
+}
